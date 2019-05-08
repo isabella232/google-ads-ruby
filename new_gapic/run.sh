18a302e889c5c735a4a08ec38c0854a2c4c83e22
Mike Moore <mike@blowmage.com>
2019-05-08 16:08:17 -0600

    Update script to generate proto and service files

--- new_gapic/run.sh
+++ new_gapic/run.sh
@@ -4,4 +4,5 @@ set -exuo pipefail
 bundle install
 git clone https://github.com/googleapis/googleapis || (cd googleapis && git reset --hard && git pull)
 mkdir out
-bundle exec protoc googleapis/google/ads/googleads/v1/services/campaign_service.proto --ruby_gapic_opt=configuration=ads.yml --proto_path googleapis/ --ruby_gapic_out=out
+mkdir out/lib
+bundle exec grpc_tools_ruby_protoc googleapis/google/ads/googleads/v1/services/campaign_service.proto --ruby_gapic_opt=configuration=ads.yml --proto_path googleapis/ --ruby_gapic_out=out --ruby_out=out/lib --grpc_out=out/lib
